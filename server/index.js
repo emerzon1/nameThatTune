69bf17bb0a6cdd7d503f3172c6b5a00b5d34f35b
Evan Merzon <evandaniel160@yahoo.com>
2021-04-03 10:34:42 -0400

    Start leaderboard

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const mongoose = require("mongoose");
+const Person = require("./models/Person");
+dotenv.config();
+const app = express();
+const url =
+	"mongodb+srv://emerzon:" +
+	process.env.DB_PASSWORD +
+	"@cluster0.t18va.mongodb.net/Leaderboard?retryWrites=true&w=majority";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+mongoose.set("bufferCommands", false);
+mongoose.connect(
+	url,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+	},
+	() => console.log("connected")
+);
+app.get("/", (req, res) => {
+	let category = req.query.category;
+	console.log(category);
+	Person.find({ category })
+		.sort({ score: -1 })
+		.limit(5)
+		.then((users) => res.json(users))
+		.catch((err) => res.status(400).json("Error: " + err));
+});
+app.post("/", async (req, res) => {
+	const user = new Person({
+		name: req.body.name,
+		score: req.body.score,
+		category: req.body.category,
+	});
+	await user.save();
+	res.send(req.body);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log());
---
+++ server/models/Person.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const personSchema = new mongoose.Schema({
+	name: { type: String, required: true, min: 0, max: 15 },
+	score: { type: Number, required: true },
+	category: { type: String, required: true },
+});
+
+module.exports = mongoose.model("People", personSchema);
